624466a8df1e7ed3430f23dffb20793c59534e35
Duncan Parkes <duncan@mysociety.org>
2015-05-13 12:35:49 +0100

    Find the URLs and depaginate.

--- scraper.py
+++ scraper.py
@@ -1,3 +1,27 @@
+from urlparse import urljoin
+import requests
+import lxml.html
+
+source_url = 'http://www.parliament.gov.na/index.php?option=com_contact&view=category&id=104&Itemid=1479'
+resp = requests.get(source_url)
+
+root = lxml.html.fromstring(resp.text)
+
+terms = [(x.find('span').text.strip(), urljoin(source_url, x.get('href')))
+         for x in root.cssselect('.menu-treemenu')[0].cssselect('a')]
+
+data = []
+
+for term_name, term_url in terms:
+
+    while term_url:
+        print term_url
+        term_resp = requests.get(term_url)
+        term_root = lxml.html.fromstring(term_resp.text)
+
+        next_links = term_root.cssselect('a[title=Next]')
+        term_url = urljoin(term_url, next_links[0].get('href')) if next_links else None
+
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
